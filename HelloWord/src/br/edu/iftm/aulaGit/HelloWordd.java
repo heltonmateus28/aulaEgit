9564186417cc14a14f9d9282fd98f7e54e068a58
Helton Mateus <helton.mateus@yahoo.com>
2015-05-28 20:22:20 -0300

    terceiro commit

--- HelloWord/src/br/edu/iftm/aulaGit/HelloWordd.java
+++ HelloWord/src/br/edu/iftm/aulaGit/HelloWordd.java
@@ -6,8 +6,9 @@ public class HelloWordd {
 	public static void main(String[] args) {
 	
 		System.out.println("Aula git segundo commit... ");
+		System.out.println("mais um commit ");
 
-		//deqdqe
+		
 	}
 
 }
